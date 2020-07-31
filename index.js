69d37f57ff29e35157456fae96e2c404653c5205
Fabrizio Henriquez <fabri.hz@hotmail.com>
2020-07-31 16:25:16 -0500

    Update 7/31/2020
    Made tab nav responsive to mobile

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ $(".tab-nav").click(function(event) {
   $("#display-col").fadeOut("fast");
 });
 
+if (screen.width < 500) {
+  $("#tab-nav-list-container").removeClass("col-4");
+  $("#tab-nav-list-container").addClass("col-12");
+  $("#display-col").removeClass("col-8");
+  $("#display-col").addClass("col-12");
+  $(".topic-menu").removeClass("col-8");
+  $(".topic-menu").addClass("col-12");
+
+}
+
 $(".topic-menu").hide();
